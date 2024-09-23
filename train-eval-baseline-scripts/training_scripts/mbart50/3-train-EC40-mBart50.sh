e92675930b526ed65c7db526adae8475c447b589
Smu-Tan <shaomu.tan.edu@gmail.com>
2024-09-23 17:40:17 +0200

    update mbart50

--- train-eval-baseline-scripts/training_scripts/mbart50/3-train-EC40-mBart50.sh
+++ train-eval-baseline-scripts/training_scripts/mbart50/3-train-EC40-mBart50.sh
@@ -21,15 +21,16 @@ source /home/stan1/anaconda3/bin/activate fairseq
 mbart_checkpoint=model_extended_EC40.pt
 
 # 2. Extend the mBart50 vocabulary and embedding:
-#    Because: a. If you add new unseen languages (beyond 50), then you need new tokens to specify those languages.
+#    Because: a. If you add new unseen languages (beyond 50), then you need new tokens to specify those languages (see how to modify lang_dict below).
 #             b. In addition, you need to expand the embedding such as encoder.embed_tokens.weight as well, e.g.: 
 #                encoder.embed_tokens.weight.shape = (250054, 1024) -> (250054+n, 1024) n donotes the new added unseen languages.
 #                to do that, you can random initialize new embedding weights or copy english weights -> both deliver similar results (in my case)
-## run extend_mbart50.ipynb (adjust it according to your need )
+## run extend_mbart50.ipynb (adjust it according to your need)
 
 
 # 3. Train
-## remember to modify lang_dict first (add new unseen languages to ML50_langs_extended_EC40.txt)
+## remember to modify lang_dict first 
+## our template is ML50_langs_extended_EC40.txt
 fairseq-train mbart-fairseq-data-bin-sharded/shard0:mbart-fairseq-data-bin-sharded/shard1:mbart-fairseq-data-bin-sharded/shard2:mbart-fairseq-data-bin-sharded/shard3:mbart-fairseq-data-bin-sharded/shard4 \
   --task translation_multi_simple_epoch \
   --finetune-from-model $mbart_checkpoint \
